6487951c0a5f7ff7c11aaaeda41fcb458355769b
Omar <omarchavezgutierrezdevelasco@gmail.com>
2024-04-04 18:55:11 -0600

    navbar and form components imported in App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar/Navbar'
+import Form from './components/form/Form'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  )
+}
+
+export default App

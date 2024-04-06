097c684e9ce755f3042f53dfd7e614fdca093833
Omar <omarchavezgutierrezdevelasco@gmail.com>
2024-04-05 20:18:27 -0600

    mysql  dependencie deleted and mysql2 install as well as cors, POST method pass recibe information from the front end and send it to the data base

--- server/index.jsx
+++ server/index.jsx
@@ -1,8 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const mysql = require('mysql2')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('hello Omar')
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'listdatabase',
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/api/insert', (req, res) => {
+  const title = req.body.title
+  const autor = req.body.autor
+  const content = req.body.content
+  const date = req.body.date
+
+  const sqlInsert =
+    'INSERT INTO blog_posts (title, autor, content, date) VALUES  (?,?,?,?)'
+  db.query(sqlInsert, [title, autor, content, date], (err, result) => {
+    console.log(result)
+    console.log(err)
+  })
 })
 
 app.listen(3001, () => {

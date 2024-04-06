2f12a64039121328a4a91010723accf7add9a94c
Omar <omarchavezgutierrezdevelasco@gmail.com>
2024-04-06 00:43:32 -0600

    context created on app.jsx file and applied to form and list component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
+import React, { useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Form from './components/form/Form'
 import List from './components/list/List'
+export const Container = React.createContext(null)
 
 function App() {
+  const [blogList, setBlogList] = useState()
   return (
-    <>
+    <Container.Provider value={{ blogList, setBlogList }}>
       <Navbar />
       <Form />
       <List />
-    </>
+    </Container.Provider>
   )
 }
 

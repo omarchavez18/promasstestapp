75c7bcbe844de6886d3ed0920380036d14ad888e
Omar <omarchavezgutierrezdevelasco@gmail.com>
2024-04-06 21:52:12 -0600

    search section moved to List component

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,9 @@
 import style from '../navbar/navbar.module.scss'
-import Button from '../button/Button'
+
 const Navbar = () => {
   return (
     <nav className={style.nav}>
       <h1>Promass test</h1>
-
-      <section className={style.sectionSearch}>
-        <form>
-          <label>Search:</label>
-          <input type='text' />
-        </form>
-        <Button type='submit'>submit</Button>
-      </section>
     </nav>
   )
 }
